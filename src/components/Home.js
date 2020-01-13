e35f39a30cda1226376d851f3fcc1e68d3b0a972
Nick <barnard.nicholas@googlemail.com>
2020-01-13 21:28:47 +0000

    tidied routing

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,7 +1,11 @@
 import React from "react";
 
 const Home = () => {
-  return <div id="home"></div>;
+  return (
+    <div id="home">
+      <h3>NC News Home Page</h3>
+    </div>
+  );
 };
 
 export default Home;
--- src/components/Viewport.js
+++ src/components/Viewport.js
@@ -2,13 +2,15 @@ import React from "react";
 import { Router } from "@reach/router";
 import ArticlesHome from "./ArticlesHome";
 import TopicsHome from "./TopicsHome";
+import Home from "./Home";
 
 const Viewport = () => {
   return (
     <div id="viewport">
       <Router>
-        <ArticlesHome path="/api/articles" />
-        <TopicsHome path="api/topics" />
+        <Home path="/" />
+        <ArticlesHome path="/articles" />
+        <TopicsHome path="/topics" />
       </Router>
     </div>
   );
