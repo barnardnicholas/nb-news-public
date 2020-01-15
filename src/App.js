6d5087b2a9b9bba26488a4b9b02f5b3c7a86d4db
Nick <barnard.nicholas@googlemail.com>
2020-01-15 11:35:34 +0000

    refactoring app for less nesting

--- src/App.js
+++ src/App.js
@@ -1,13 +1,27 @@
 import React from "react";
 import Overlay from "./components/Overlay";
-import MasterWindow from "./components/MasterWindow";
+import WindowBar from "./components/WindowBar";
+import Header from "./components/Header";
+import NavBar from "./components/NavBar";
+import SideBar from "./components/SideBar";
+import Viewport from "./components/Viewport";
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <Overlay />
-      <MasterWindow />
+      <div id="masterwindow">
+        <WindowBar />
+        <div id="windowcontent">
+          <Header />
+          <NavBar />
+          <div id="main">
+            <SideBar />
+            <Viewport />
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
